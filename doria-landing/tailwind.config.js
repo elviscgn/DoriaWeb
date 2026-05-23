be50e8c0c2c1cf73ad95c6ce5c0897ab96043482
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-05-23 15:43:01 +0200

    configure tailwind css

---
+++ doria-landing/tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+    content: ["./index.html", "./src/**/*.{ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: { sora: ["Sora", "sans-serif"] },
+        },
+    },
+    plugins: [],
+}
